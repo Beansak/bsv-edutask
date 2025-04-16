72b9448aeae9abf19663df0cc075b87f3e9dc736
Isak Klaesson <isak.klaesson2002@gmail.com>
2025-04-16 13:16:35 +0200

    Finished 2 of 5 tests

--- backend/test/Unit/test_usercontroller.py
+++ backend/test/Unit/test_usercontroller.py
@@ -11,26 +11,25 @@ class TestUserController:
         
         # Mock the email object
         email = 'jane.doe'
-
-        with patch('src.util.dao.DAO') as mockedDAO:
-            mock = MagicMock()
-            mockedDAO.return_value = mock
-            uc = UserController(dao=mockedDAO)
-            with pytest.raises(ValueError, match="Error: invalid email address"):
-                uc.get_user_by_email(email)
+        # Mock the DAO to return a simulated user
+        mockedDAO = MagicMock()
+        
+        uc = UserController(dao=mockedDAO)
+        with pytest.raises(ValueError, match="Error: invalid email address"):
+            uc.get_user_by_email(email)
 
     def test_valid_email_adress(self):
         """Test the get_user_by_email method with a valid email address.
         """
 
-        user = {'firstName': 'Jane', 'lastName': 'Doe', 'email': 'jane.doe'}
-        email = "jane.doe@email.com"
+        user = {'firstName': 'Jane', 'lastName': 'Doe', 'email': 'jane.doe@gmail.com'}
+        email = "jane.doe@gmail.com"
         mockedDAO = MagicMock()
-        mockedDAO.find.return_value = [user]  # Simulate the DAO's find() method
+        mockedDAO.find.return_value = [{'firstName': 'Jane', 'lastName': 'Doe', 'email': 'jane.doe@gmail.com'}]  # Simulate the DAO's find() method
         # Mock the DAO to return a simulated user
         uc = UserController(dao=mockedDAO)
         # Mock the user object
-        # Patch the fullmatch method of the regex library
-        # Mock the fullmatch method to always return True
         
-        assert uc.get_user_by_email(email) == user
+        with patch('re.fullmatch') as mockfullmatch:
+            mockfullmatch.return_value = True
+            assert uc.get_user_by_email(email) == user
